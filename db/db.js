b5020493849665f1afbdf505101ecab93b035c71
Christi Harlow <cah120671@gmail.com>
2022-08-18 20:06:20 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,8 +1,26 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://christiharlow@localhost:5432/favorites", {
-    logging: false,
-});
+let options = {};
+let databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+    // this means we're on localhost!
+    (databaseURL = "postgres://christiharlow@localhost:5432/favorites"),
+        (options = {
+            logging: false,
+        });
+} else {
+    // we're not on localhost!
+    options = {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    };
+}
+
 const Favorites = require("./Favorites")(db);
 
 const connectToDB = async () => {
