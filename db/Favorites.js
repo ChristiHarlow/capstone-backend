4cc1e62e88366d36109ae6e03319b0e5739ecc4e
ChristiHarlow <cah120671@gmail.com>
2024-02-16 11:08:24 -0500

    Updates
    Attempting to correct 502 error

--- db/Favorites.js
+++ db/Favorites.js
@@ -1,49 +1,51 @@
-const { DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
-module.exports = (sequelize) => {
-  const Favorites = sequelize.define(
-    "favorites",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING(255), // Example max length
-        allowNull: false, // Assuming name is required
-      },
-      category: {
-        type: DataTypes.STRING(255),
-        allowNull: true, // Assuming category can be optional
-      },
-      summary: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      imageURL: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2), // Assuming price is a decimal value
-        allowNull: true,
-      },
-      links: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      sort: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-    },
-    {
-      // Other model options here
-    }
-  );
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-  return Favorites;
+module.exports = (sequelize, DataTypes) => {
+    const Favorites = sequelize.define(
+        "Favorites",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: DataTypes.STRING(255), // Example max length
+                allowNull: false, // Assuming name is required
+            },
+            category: {
+                type: DataTypes.STRING(255),
+                allowNull: true, // Assuming category can be optional
+            },
+            summary: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            imageURL: {
+                type: DataTypes.STRING(255),
+                allowNull: true,
+            },
+            price: {
+                type: DataTypes.DECIMAL(10, 2), // Assuming price is a decimal value
+                allowNull: true,
+            },
+            links: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            sort: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+        },
+        {
+            // Other model options here
+        }
+    );
+
+    return Favorites;
 };
 
 module.exports = Favorites;
