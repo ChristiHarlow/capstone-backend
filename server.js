26e7254ab54828e6fab254b797e35cd463fa2ae3
Christi Harlow <cah120671@gmail.com>
2024-03-20 04:39:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3020;
 
 app.get("/", (req, res) => {
     res.send("Hello from server.js!");
