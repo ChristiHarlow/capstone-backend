400b1502efcd87ff91a143570a67ef3fea42ebb6
Christi Harlow <cah120671@gmail.com>
2024-03-20 03:47:41 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -8,7 +8,10 @@ const sequelizeOptions = {
     dialect: "postgres",
     logging: false, // Adjust logging as needed
     dialectOptions: {
-        ssl: false
+        ssl: {
+            require: false,
+            rejectUnauthorized: false
+        }
     }
     
 };
