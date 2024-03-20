f7de18f5c17970f85eab5b54c0ce62c1583c1786
Christi Harlow <cah120671@gmail.com>
2024-03-20 04:26:25 -0400

    Update test-db.js

--- db/test-db.js
+++ db/test-db.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Ensure the environment variable is being read correctly
-const databaseUrl = process.env.LOCAL_DATABASE_URL;
-console.log('Database URL:', databaseUrl);
+const databaseUrl = 'postgres://christiharlow:Kiara219@localhost:5432/favorites';
 
-// Initialize Sequelize with the connection string
 const sequelize = new Sequelize(databaseUrl, {
     dialect: 'postgres',
     logging: console.log, // Enable logging for debugging purposes
