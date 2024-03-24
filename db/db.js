1ca7f68193d11118d457e7d76dd4bab733f04b23
Christi Harlow <cah120671@gmail.com>
2024-03-24 15:01:28 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,9 +3,11 @@ const { Sequelize } = require("sequelize");
 
 // Use Cloud SQL Proxy connection string format if running on GAE
 const isAppEngine = process.env.INSTANCE_CONNECTION_NAME != null;
-const databaseUrl = isAppEngine 
-    ? `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}/${process.env.DB_NAME}`
-    : process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+// const databaseUrl = isAppEngine 
+//     ? `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}/${process.env.DB_NAME}`
+//     : process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+
+const databaseUrl = "postgres://favorite-things-120671:us-east5:favorites-capstone" 
 
 const sequelize = new Sequelize(databaseUrl, {
     dialect: 'postgres',
