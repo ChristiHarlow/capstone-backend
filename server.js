d2b4b586809e5bee6dce678208324141037c92d1
Christi Harlow <cah120671@gmail.com>
2024-03-27 13:35:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,21 +12,27 @@ const Favorites = require("./models/Favorites");
 
 const app = express();
 
+// Enable trust proxy to trust headers set by proxies
+app.set('trust proxy', true);
+
 // Middleware
 app.use(cors()); // Apply CORS with default settings
 app.use(helmet());
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(
-    rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // Limit each IP to 100 requests per windowMs
-    })
-);
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later." // Optional message to be sent when the limit is exceeded
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Define the root route
-app.get('/', (req, res) => {
+app.get('/debugonly', (req, res) => {
     res.send('Welcome to the application!');
 });
 
@@ -48,6 +54,11 @@ app.use((err, req, res, next) => {
     res.status(500).json({error: "Internal Christi2Global Server Error", details: err.message});
 });
 
+// Example route
+app.get('/api', (req, res) => {
+    res.send('Hello, World!');
+  });
+  
 // Start the server
 const PORT = process.env.PORT || 3020;
 
